c27c419d02864ae61c54257111b2d9c0da2a59f1
Augusto Langer <augusto.langer@gmail.com>
2024-08-17 16:51:19 +0200

    first working tests

--- src/hash_map.hpp
+++ src/hash_map.hpp
@@ -1,5 +1,8 @@
 #include <functional>
+#include <optional>
 #include <list>
+
+#include <iostream>
 
 namespace hash_map_example
 {
@@ -12,25 +15,43 @@ public:
     using HashFunction = std::function<int(T_Key)>;
     using Bucket = std::list<KeyValuePair>;
 
-    HashMap();
-    ~HashMap();
+    HashMap(int num_buckets = 100, HashFunction hash_function = std::hash<T_Key>())
+    :   hash_function(hash_function),
+        num_buckets(num_buckets),
+        buckets(num_buckets)
+    {
+    }
+    ~HashMap()
+    {
+        std::cout << "Destructor called" << std::endl;
+    }
 
     void insert(const T_Key& key, const T_Value& value)
     {
-        auto bucket = get_bucket(key);
+        auto& bucket = get_bucket(key);
         for (auto& pair : bucket)
         {
             if (pair.first == key)
             {
                 pair.second = value;
+                last_key = key;
                 return;
             }
         }
         bucket.push_back(KeyValuePair(key, value));
+        if (!first_key)
+        {
+            first_key = key;
+            std::cout << "First key: " << *first_key << std::endl;
+            std::cout << "Insert, has first" << first_key.has_value() << std::endl;
+        }
+        last_key = key;
+
     }
 
     T_Value get(const T_Key& key) const
     {
+        std::cout << "Has first" << first_key.has_value() << std::endl;
         auto& bucket = get_bucket(key); 
         for (auto& pair : bucket)
         {
@@ -43,25 +64,60 @@ public:
         throw std::out_of_range("Key not found");
     }
 
-    void remove(int key);
+    void remove(const T_Key& key)
+    {
+        auto& bucket = get_bucket(key);
+        for (auto it = bucket.begin(); it != bucket.end(); ++it)
+        {
+            if (it->first == key)
+            {
+                bucket.erase(it);
+                return;
+            }
+        }
+    }
+
+    KeyValuePair get_first() const
+    {
+        if (!first_key)
+        {
+            throw std::out_of_range("No elements inserted or modified");
+        }
+        return KeyValuePair(*first_key, get(*first_key));
+    }
+
+    KeyValuePair get_last() const
+    {
+        if (!last_key)
+        {
+            throw std::out_of_range("No elements inserted or modified");
+        }
+        return KeyValuePair(*last_key, get(*last_key));
+    }
+
 
 private:
 
     Bucket& get_bucket(const T_Key& key)
     {
         size_t index = (hash_function(key) % num_buckets + num_buckets) % num_buckets;
+        std::cout << "Non-const get_bucket " << index << " " << num_buckets << std::endl;
         return buckets[index];
     }
 
     const Bucket& get_bucket(const T_Key& key) const
     {
         size_t index = (hash_function(key) % num_buckets + num_buckets) % num_buckets;
+        std::cout << "Const get_bucket " << index << " " << num_buckets << std::endl;
         return buckets[index];
     }
 
     HashFunction hash_function;
     const size_t num_buckets;
     std::vector<Bucket> buckets;
+
+    std::optional<T_Key> first_key = std::nullopt;
+    std::optional<T_Key> last_key = std::nullopt;
 
 };
 
--- test/test_hash_map.cpp
+++ test/test_hash_map.cpp
@@ -1,11 +1,72 @@
 #include <gtest/gtest.h> // Include the Google Test framework
 #include <hash_map.hpp> // Include the header file for the class we are testing
 
+class HashMapTest : public ::testing::Test
+{
+protected:
 
-// Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+    HashMapTest()
+    :   map()
+    {
+
+    }
+
+    ~HashMapTest() = default;
+
+    // void SetUp() override
+    // {
+    //     map = std::make_shared<hash_map_example::HashMap<std::string, int>>();
+    // }
+
+    hash_map_example::HashMap<std::string, int> map;
+};
+
+// // Demonstrate some basic assertions.
+// TEST_F(HashMapTest, InsertAndRetrieve)
+// {
+//     EXPECT_THROW(map->get_first(), std::out_of_range);
+//     EXPECT_THROW(map->get_last(), std::out_of_range);
+
+//     map->insert("one", 1);
+//     EXPECT_EQ(map->get("one"), 1);
+//     // EXPECT_EQ(map->get_first().first, "one");
+//     // EXPECT_EQ(map->get_last().first, "one");
+ 
+//     map->insert("two", 2);
+//     EXPECT_EQ(map->get("one"), 1);
+// EXPECT_EQ(map->get("two"), 2);
+//     // EXPECT_EQ(map->get_first().first, "one");
+//     EXPECT_EQ(map->get_last().first, "two");
+
+
+//     // map.insert("three", 3);
+
+//     // EXPECT_EQ(map.get("two"), 2);
+//     // EXPECT_EQ(map.get("three"), 3);
+// }
+
+TEST(MyHashMapTest, BasicFunctionality)
+{
+    hash_map_example::HashMap<std::string, int> map;
+    EXPECT_THROW(map.get_first(), std::out_of_range);
+    EXPECT_THROW(map.get_last(), std::out_of_range);
+
+    map.insert("one", 1);
+    EXPECT_EQ(map.get("one"), 1);
+    EXPECT_EQ(map.get_first().first, "one");
+    EXPECT_EQ(map.get_last().first, "one");
+ 
+    map.insert("two", 2);
+    EXPECT_EQ(map.get("one"), 1);
+    EXPECT_EQ(map.get("two"), 2);
+    EXPECT_EQ(map.get_first().first, "one");
+    EXPECT_EQ(map.get_last().first, "two");
+
+    map.insert("one", -1);
+    EXPECT_EQ(map.get("one"), -1);
+    EXPECT_EQ(map.get("two"), 2);
+    EXPECT_EQ(map.get_last().first, "one");
+
+    map.remove("two");
+    EXPECT_THROW(map.get("two"), std::out_of_range);
 }
